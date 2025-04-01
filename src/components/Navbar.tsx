354ba33d4c4581a5a0f5a0b2d22a7f0e983d2c91
HenujaKa <katuwapitiyah@gmail.com>
2025-04-01 20:32:09 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar: React.FC = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className={`nav-link ${location.pathname === `/` ? "active" : ""}`} to={`/`}>
+              <Link className={`nav-link ${location.pathname === `/PS_portfolio` ? "active" : ""}`} to={`/`}>
                 Home
               </Link>
             </li>
